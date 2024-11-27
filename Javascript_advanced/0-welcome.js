cb54cf4edf44605a254d2f0f22465edf0e76db40
ksyv <viais.kevin@hotmail.fr>
2024-11-27 12:16:30 +0100

    add 0 with the welcome function for display full name with a first name and a last name in arg

---
+++ Javascript_advanced/0-welcome.js
@@ -0,0 +1,8 @@
+function welcome(firstName, lastName) {
+    var fullName = firstName + " " + lastName;
+  
+    function displayFullName() {
+      alert("Welcome " + fullName + "!");
+    }
+  displayFullName();
+}
